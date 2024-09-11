810b70b06470be72b99574ec3d1671dd0e848132
jdkim <jindong.kim@gmail.com>
2024-09-11 14:34:31 +0900

    The 'fix' mode is implemented to fix invalid annotations.

--- pubtator-to-pubannotation.rb
+++ pubtator-to-pubannotation.rb
@@ -2,11 +2,14 @@
 require 'ox'
 require 'json'
 
-def PubTatorBioC_to_PubAnnotationJSON(xml_file, validate_p = false)
-	pubannotation_docs = []
+def PubTatorBioC_to_PubAnnotationJSON(xml_file, mode = nil)
+	mode ||= :go
+	annotations_count = 0
+	invalids_count = 0
+	fix_count = 0
+	skip_count = 0
 
 	parsed_xml = Ox.parse(xml_file)
-
 	parsed_xml.locate('collection/document').each do |doc|
 		docid = doc.locate('id').first.text
 
@@ -20,6 +23,8 @@ def PubTatorBioC_to_PubAnnotationJSON(xml_file, validate_p = false)
 			offset = passage.locate('offset').first.text.to_i
 
 			passage.locate('annotation').each do |annotation|
+				annotations_count += 1
+
 				id = annotation.attributes[:id]
 				obj_id   = annotation.locate("infon[@key=identifier]").first&.text
 				obj_type = annotation.locate("infon[@key=type]").first.text
@@ -28,8 +33,32 @@ def PubTatorBioC_to_PubAnnotationJSON(xml_file, validate_p = false)
 				s_end = s_beg + location.attributes[:length].to_i
 				lex = annotation.locate('text').first.text
 
-				if validate_p
-					warn "[#{docid}:#{id}] WARNING text mismatch (#{s_beg}, #{s_end}) : [#{text[s_beg ... s_end]}] vs [#{lex}]" if text[s_beg ... s_end] != lex
+				# handling invalid annotations
+				if mode != :go && text[s_beg ... s_end] != lex
+					invalids_count += 1
+
+					fixed = false
+					if mode == :fix
+						# invalid annotations are fixed and counted
+						adjustment = get_adjustment(text, s_beg, s_end, lex)
+						unless adjustment.nil?
+							s_beg += adjustment
+							s_end += adjustment
+							fixed = true
+							fix_count += 1
+						end
+					end
+
+					unless fixed
+						if mode == :skip
+							# invalid annotations are counted and skipped
+							skip_count += 1
+							next
+						elsif mode == :report
+							# invalid annotations are reported
+							warn "[#{docid}:#{id}] WARNING text mismatch (#{s_beg}, #{s_end}) : [#{text[s_beg ... s_end]}] vs [#{lex}]" if text[s_beg ... s_end] != lex
+						end
+					end
 				end
 
 				denotations << {id: id, span: {begin: s_beg, end: s_end}, obj: obj_type}
@@ -39,15 +68,22 @@ def PubTatorBioC_to_PubAnnotationJSON(xml_file, validate_p = false)
 			next if denotations.empty?
 			raise "Invalid passage. Annotations exist but text does not exist." if text.nil?
 			annotations = {sourcedb:'PubMed', sourceid: docid, text: text, denotations: denotations, attributes: attributes}
-			yield annotations
+			yield annotations, annotations_count, invalids_count, fix_count, skip_count
 		end
 	end
+end
+
+def get_adjustment(text, s_beg, s_end, lex)
+	window_size = 5
+	window = text[(s_beg - window_size) ... (s_end - 1)]
+	r = window.rindex(lex)
+	r.nil? ? nil : r - window_size
 end
 
 
 if __FILE__ == $0
 	odir = 'output'
-	validate_p = false
+	mode = :go # the behavior for invalid annotations. Options include :report, :skip, :fix, or :go (default)
 
 	## command line option processing
 	require 'optparse'
@@ -59,8 +95,16 @@ if __FILE__ == $0
 			odir.sub(%r|/+|, '')
 		end
 
-		opts.on('-v', '--validate', 'tells it to validate annotation during the conversion') do
-			validate_p = true
+		opts.on('-v', '--validate', 'tells it to validate annotations during the conversion') do
+			mode = :report
+		end
+
+		opts.on('-s', '--skip', 'tells it to skip invalid annotations during the conversion') do
+			mode = :skip
+		end
+
+		opts.on('-f', '--fix', 'tells it to try to fix invalid annotations during the conversion') do
+			mode = :fix
 		end
 
 		opts.on('-h', '--help', 'displays this screen') do
@@ -85,13 +129,27 @@ if __FILE__ == $0
 		xml_file = File.read(f)
 		puts "processing #{f}"
 
-    filebase = File.basename(f, "XML")
+		total_annotations = 0
+		total_invalids = 0
+		total_fixed = 0
+		total_skipped = 0
+
+		filebase = File.basename(f, "XML")
 		outfilename = "#{filebase}jsonl"
 		outfilepath = File.join(odir, outfilename) unless odir.nil?
 		File.open(outfilepath, 'w') do |outfile|
-			PubTatorBioC_to_PubAnnotationJSON(xml_file, validate_p) do |annotations|
+			PubTatorBioC_to_PubAnnotationJSON(xml_file, mode) do |annotations, annotations_count, invalids_count, fixed_count, skipped_count|
 				outfile.write(annotations.to_json + "\n")
+				total_annotations += annotations_count
+				total_invalids += invalids_count
+				total_fixed += fixed_count
+				total_skipped += skipped_count
 			end
 		end
+
+		puts "    Total annotation: #{total_annotations}"
+		puts "    Invalid annotations: #{total_invalids} (#{100 * total_invalids.to_f/total_annotations}%)"
+		puts "    Fixed annotations: #{total_fixed} (#{100 * total_fixed.to_f/total_invalids}%)"
+		puts "    Skipped_annotations: #{total_skipped}"
 	end
 end
